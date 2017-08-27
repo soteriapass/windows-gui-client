791671ae5b26960ba177f83c07dfeb6f71f5ef89
viperman1271 <viperman1271@hotmail.com>
2017-08-27 12:22:12 -0400

    Error handling on launch
    Added some generic error handling on launch, in case there were some
    issues trying to create the view model or initialize the view for the
    MainView

--- src/Views/MainView.xaml.cs
+++ src/Views/MainView.xaml.cs
@@ -1,4 +1,7 @@
-﻿namespace PasswordManager
+﻿using System;
+using System.Windows;
+
+namespace PasswordManager
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -15,10 +18,30 @@
 
         public MainView()
         {
-            _ViewModel = new MainViewModel(this);
-            DataContext = _ViewModel;
+            try
+            {
+                _ViewModel = new MainViewModel(this);
+                DataContext = _ViewModel;
 
-            InitializeComponent();
+                InitializeComponent();
+            }
+            catch(Grpc.Core.RpcException ex)
+            {
+                if(ex.Status.StatusCode == Grpc.Core.StatusCode.Unavailable)
+                {
+                    MessageBox.Show("The remote service is currently unavailable", "Could not connect", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+                else
+                {
+                    MessageBox.Show($"An unknown communications error was encountered.\r\n\r\n{ex.Message}", "Could not connect", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+                throw;
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show($"An unknown error was detected\r\n{ex.Message}", "Unknown Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                throw;
+            }
         }
 
         #endregion
